e6ecaeb6eccc7a0f2e226847a25daa176c8138fb
Raneem <s-raneem.megahed@zewailcity.edu.eg>
2022-12-17 15:09:30 +0200

    added load operation

---
+++ operations/opLoad.cpp
@@ -0,0 +1,51 @@
+#include "opLoad.h"
+#include "..\Shapes\Shape.h"
+
+#include "..\controller.h"
+
+#include "..\GUI\GUI.h"
+
+opLoad::opLoad(controller* pCont):operation(pCont)
+{
+	LFig = NULL;
+}
+
+opLoad::~opLoad()
+{
+}
+
+void opLoad::Execute()
+{
+	GUI* pUI = pControl->GetUI();
+	Graph* pGr = pControl->getGraph();
+	shape* bw = pGr->GetSelected();
+	File.open(FileName, ios::in);
+	int Figcount;
+
+	if (File.is_open())     //Check if There is a File with this Name or Not
+	{
+		pUI->PrintMessage("enter file name");
+		pUI->ClearDrawArea();
+		string Fig;
+		int FCount;
+
+		//Load Figures Count	
+		File >> FCount;
+
+		
+
+			LFig->load(File);
+			
+			LFig = NULL;
+
+		
+
+		File.close();
+		pUI->ClearStatusBar();
+		pUI->PrintMessage("File is Loaded Successfully");
+	}
+
+	else
+		pUI->PrintMessage("Error: Could not open the file!");{
+}
+
